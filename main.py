ffcd92e54b8ae57acd3b6e8f7ae366952d20afcb
henriquekutnercordeiro <hkutner@ciandt.com>
2024-02-16 09:31:11 -0300

    test 123

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ projects = jira.projects()
 
 keys = sorted(project.key for project in projects)[11]
 
-commit_message = os.environ.get("COMMIT_MESSAGE")
-print(commit_message)
+commit_messages = os.environ.get("COMMIT_MESSAGES")
+if commit_messages:
+    commit_messages = commit_messages.split("\n")
+    for commit_message in commit_messages:
+        print(commit_message)
 
 # issue_key = commit_message.split(':')[0].strip('[').strip(']')
 
