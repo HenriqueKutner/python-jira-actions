ba90b0040336861a18603d7dd0cb8e3e51a945e5
henriquekutnercordeiro <hkutner@ciandt.com>
2024-02-16 10:58:58 -0300

    [GOPS-3098]: add new feature

--- main.py
+++ main.py
@@ -12,20 +12,11 @@ projects = jira.projects()
 
 keys = sorted(project.key for project in projects)[11]
 
-commit_messages = os.environ.get("COMMIT_MESSAGES")
+commit_messages = os.environ.get("COMMIT_MESSAGE")
 
-if commit_messages:
-    # Split the commit messages by newline character
-    commit_messages_list = commit_messages.split("\n")
-    
-    # Print each commit message
-    for message in commit_messages_list:
-        print("Commit Message:", message)
-else:
-    print("No commit messages found.")
 
-# issue_key = commit_messages.split(':')[0].strip('[').strip(']')
-# issue = jira.issue(issue_key)
+issue_key = commit_messages.split(':')[0].strip('[').strip(']')
+issue = jira.issue(issue_key)
 
-# print("Título da issue = ", issue.fields.summary)
-# print("Descrição da issue = ", issue.fields.description)
+print("Título da issue = ", issue.fields.summary)
+print("Descrição da issue = ", issue.fields.description)
